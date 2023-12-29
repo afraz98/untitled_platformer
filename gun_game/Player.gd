7aa9f12f24ff1a7fd93621ed3d1a2abc3ef2c47f
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-28 21:02:00 -0500

    Add collision to TileMap

--- gun_game/Player.gd
+++ gun_game/Player.gd
@@ -1,14 +1,23 @@
-extends CharacterBody2D
-class_name Player
+class_name Player extends CharacterBody2D
+
 
 const WALK_FORCE = 600
 const WALK_MAX_SPEED = 200
 const STOP_FORCE = 1300
-const JUMP_SPEED = 200
+const JUMP_SPEED = 500
 
 @onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
+	if is_on_floor():
+		$PlayerSprite.animation = "walk"
+		$PlayerSprite.flip_v = false
+		$PlayerSprite.flip_h = velocity.x < 0
+	elif velocity.y > 0:
+		$PlayerSprite.animation = "up"
+		$PlayerSprite.flip_v = velocity.y > 0
+
+
 	# Horizontal movement code. First, get the player's input.
 	var walk = WALK_FORCE * (Input.get_axis(&"move_left", &"move_right"))
 	
@@ -31,3 +40,4 @@ func _physics_process(delta):
 	# Check for jumping. is_on_floor() must be called after movement code.
 	if is_on_floor() and Input.is_action_just_pressed(&"jump"):
 		velocity.y = -JUMP_SPEED
+
