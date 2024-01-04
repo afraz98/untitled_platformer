dc7e42a49037ada297240248ae9cf21e0ee1ab68
afraz98 <anthonymfrazier1998@gmail.com>
2024-01-03 23:09:42 -0500

    Added crouching hitbox for player

--- untitled_platformer/Player.gd
+++ untitled_platformer/Player.gd
@@ -2,11 +2,20 @@ class_name Player extends CharacterBody2D
 
 const WALK_FORCE = 600
 const WALK_MAX_SPEED = 200
+
+const CROUCH_FORCE = 300
+const CROUCH_MAX_SPEED = 100
+
 const STOP_FORCE = 1300
 const JUMP_SPEED = 500
 
 @onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var _animation_player = $AnimationPlayer
+
+func is_crouching():
+	if Input.is_action_pressed("crouch"):
+		return true
+	return false
 
 func get_new_animation():
 	if is_on_floor():
@@ -23,24 +32,48 @@ func get_new_animation():
 		return "jump"
 	return "idle" # By default return the idle animation
 
+func update_hitbox():
+	if is_crouching():
+		$PlayerHitbox.disabled = true
+		$PlayerCrouchHitbox.disabled = false
+	else:
+		$PlayerHitbox.disabled = false
+		$PlayerCrouchHitbox.disabled = true
+	pass
+
 func _physics_process(delta):
 	_animation_player.play(get_new_animation())
+	update_hitbox()
 		
 	if velocity.x != 0:
 		$PlayerSprite.flip_h = velocity.x < 0
-	
-	# Horizontal movement code. First, get the player's input.
-	var walk = WALK_FORCE * (Input.get_axis(&"move_left", &"move_right"))
-	
-	# Slow down the player if they're not trying to move.
-	if abs(walk) < WALK_FORCE * 0.2:
-		# The velocity, slowed down a bit, and then reassigned.
-		velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
+
+	if is_crouching(): # Player should move at half speed if crouched
+		# Horizontal movement code. First, get the player's input.
+		var walk = CROUCH_FORCE * (Input.get_axis(&"move_left", &"move_right"))
+		
+		# Slow down the player if they're not trying to move.
+		if abs(walk) < CROUCH_FORCE * 0.2:
+			# The velocity, slowed down a bit, and then reassigned.
+			velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
+		else:
+			velocity.x += walk * delta
+		
+		# Clamp to the maximum horizontal movement speed.
+		velocity.x = clamp(velocity.x, -CROUCH_MAX_SPEED, CROUCH_MAX_SPEED)		
 	else:
-		velocity.x += walk * delta
-	
-	# Clamp to the maximum horizontal movement speed.
-	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
+		# Horizontal movement code. First, get the player's input.
+		var walk = WALK_FORCE * (Input.get_axis(&"move_left", &"move_right"))
+		
+		# Slow down the player if they're not trying to move.
+		if abs(walk) < WALK_FORCE * 0.2:
+			# The velocity, slowed down a bit, and then reassigned.
+			velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
+		else:
+			velocity.x += walk * delta
+		
+		# Clamp to the maximum horizontal movement speed.
+		velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
 
 	# Vertical movement code. Apply gravity.
 	velocity.y += gravity * delta
