65c9cec063c03e7c037ea91cc0db62e344a6ba00
afraz98 <anthonymfrazier1998@gmail.com>
2024-01-17 23:26:15 -0500

    Start working on enemy logic

--- untitled_platformer/Enemy.gd
+++ untitled_platformer/Enemy.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 @onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var _animation_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
+func get_new_animation():
+	return "walk"
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	_animation_player.play(get_new_animation())
 	pass
